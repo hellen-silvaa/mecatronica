fc8718f7a19a4bbc683791bbf11a0156e63a6545
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-05-31 22:31:43 -0300

    Create display.cpp

---
+++ display.cpp
@@ -0,0 +1,215 @@
+// C++ code
+//
+
+int ZERO= 0;
+int UM= 1;
+int DOIS= 2;
+int TRÊS= 3;
+int QUATRO= 4;
+int CINCO= 5;
+int SEIS= 6;
+int SETE= 7;
+int OITO= 8;
+int NOVE= 9;
+
+
+void setup()
+{
+   for(int =0; ±<9;±++)
+   {
+     pinMode (i, OUTPUT);
+   }
+}
+
+void loop()
+{
+   for(int cnt=0; cnt<9;cnt++)
+   {
+     if(cnt==0)
+     {
+       ZERO();
+     }
+          if(cnt==0)
+     {
+       UM();
+     }
+          if(cnt==0)
+     {
+       DOIS();
+     }
+          if(cnt==0)
+     {
+       TRÊS();
+     }
+          if(cnt==0)
+     {
+       QUATRO();
+     }
+          if(cnt==0)
+     {
+       CINCO();
+     }
+          if(cnt==0)
+     {
+       SEIS();
+     }
+          if(cnt==0)
+     {
+       SETE();
+     }
+          if(cnt==0)
+     {
+       OITO();
+     }
+          if(cnt==0)
+     {
+       NOVE();
+     }
+     
+     delay(500);
+   ]
+}
+
+
+void ZERO ()
+{
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+
+void UM ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+  
+void DOIS ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+  
+void TRÊS ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+  
+void QUATRO ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+  
+void CINCO ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+  
+void SEIS ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+  
+void SETE ()
+{
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+}
+  
+void OITO ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+  }
+  
+  void NOVE ()
+  {
+digitalWrite (0,1)
+digitalWrite (1,1)
+digitalWrite (2,1)
+digitalWrite (3,0)
+digitalWrite (4,0)
+digitalWrite (5,0)
+digitalWrite (6,0)
+digitalWrite (7,0)
+digitalWrite (8,0)
+digitalWrite (9,0)
+}
+  
+  
+ 
