57d5b7d6065d828f3d27b812954cd2305214b34c
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-05-31 22:22:38 -0300

    Create sequencia_led.cpp

---
+++ sequencia_led.cpp
@@ -0,0 +1,89 @@
+void setup ()
+{
+  pinMode (0, OUTPUT);
+  pinMode (1, OUTPUT);
+  pinMode (2, OUTPUT);
+  pinMode (3, OUTPUT);
+  pinMode (4, OUTPUT);
+  pinMode (5, OUTPUT);
+  pinMode (6, OUTPUT);
+  pinMode (7, OUTPUT);
+}
+
+void loop ()
+{
+  digitalWrite(0, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(0, LOW);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(1, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(1, LOW);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(2, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(2, LOW);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(3, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(3, LOW);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(4, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(4,LOW);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(5,HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(5,LOW);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(6,HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(6,LOW);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(7,HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(7,LOW);
+  
+  
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(7,HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(7,LOW);
+  
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(6,HIGH);
+  delay(100); //spera 500 millisecond (s)
+  digitalWrite(6,LOW);
+  
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(5,HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(5,LOW);
+  
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(4,HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(4,LOW);
+  
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(3,HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(3,LOW);
+  
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(2, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(2,LOW);
+  delay(100); //espera 500 millisecond (s)
+
+  digitalWrite(1, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(1,LOW);
+  delay(100); //espera 500 millisecond (s)
+
+  digitalWrite(0, HIGH);
+  delay(100); //espera 500 millisecond (s)
+  digitalWrite(0,LOW);
+  delay(100); //espera 500 millisecond (s)  
+}
+
